7688a6ac24371b489fce306fcaaea672e2e3b2f9
Diana Kovacheva <dmarinova@outlook.com>
2024-10-08 13:07:55 +0200

    Add search url

--- 02-Project-1-Wiki/wiki/encyclopedia/urls.py
+++ 02-Project-1-Wiki/wiki/encyclopedia/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 
 from . import views
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/new_entry", views.new_entry, name="new_entry"),
     path("wiki/random", views.get_random, name="get_random"),
+    path("wiki/search", views.search_entry, name="q"),
     path("wiki/<str:entry>", views.get_entry, name="entry"),
     path("wiki/<str:title>/edit_entry", views.edit_entry, name="edit_entry"),
 ]
