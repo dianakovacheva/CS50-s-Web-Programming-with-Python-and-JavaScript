7350549d55912fd8f1d3194e10ba612bda3e92fa
Diana Kovacheva <dmarinova@outlook.com>
2024-10-17 14:32:41 +0200

    Update project urls

--- 03-Project-2-Commerce/commerce/auctions/urls.py
+++ 03-Project-2-Commerce/commerce/auctions/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path("register", views.register, name="register"),
     path("create_listing", views.create_listing, name="create_listing"),
     path("categories", views.get_categories, name="categories"),
+    path("categories/<int:id>", views.get_category, name="category"),
     path("listing/<int:id>", views.get_listing, name="get_listing"),
-    path("listing/<int:id>", views.place_bid, name="place_bid")
+    path("listing/<int:id>", views.place_bid, name="place_bid"),
+    path("watchlist", views.get_watchlist, name="get_watchlist")
 ]
