e2d8adfaefad282b14055e02c7e279d26f5a09e2
Diana Kovacheva <dmarinova@outlook.com>
2024-10-25 10:40:44 +0200

    Change related_name of the Bid model

--- 03-Project-2-Commerce/commerce/auctions/models.py
+++ 03-Project-2-Commerce/commerce/auctions/models.py
@@ -33,9 +33,9 @@ class Listing(models.Model):
 class Bid(models.Model):
     bid = models.DecimalField(max_digits=10, decimal_places=2)
     # foreign key AuctionListing
-    listing = models.ForeignKey("Listing", on_delete=models.CASCADE, related_name="auction_bid")
+    listing = models.ForeignKey("Listing", on_delete=models.CASCADE, related_name="bids")
     # foreign key User
-    owner = models.ForeignKey("User", on_delete=models.CASCADE, related_name="bid_owner")
+    owner = models.ForeignKey("User", on_delete=models.CASCADE, related_name="bids")
 
     def __str__(self):
         return f"{self.bid} {self.listing} {self.owner}"
