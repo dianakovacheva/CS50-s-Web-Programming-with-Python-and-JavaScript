67ae681b3c29f47b08da91093280cd03facfc733
Diana Kovacheva <dmarinova@outlook.com>
2024-10-28 13:07:54 +0100

    Add close_listing path

--- 03-Project-2-Commerce/commerce/auctions/urls.py
+++ 03-Project-2-Commerce/commerce/auctions/urls.py
@@ -10,9 +10,9 @@ urlpatterns = [
     path("create_listing", views.create_listing, name="create_listing"),
     path("listing/<int:id>", views.get_listing, name="get_listing"),
     path("listing/<int:id>/place_bid", views.place_bid, name="place_bid"),
+    path("listing/<int:id>/close_listing", views.close_listing, name="close_listing" ),
     path("categories", views.get_categories, name="categories"),
     path("categories/<int:id>", views.get_category, name="category"),
     path("listing/<int:id>/add_to_watchlist", views.add_to_watchlist, name="add_to_watchlist"),
     path("watchlist", views.get_watchlist, name="get_watchlist"),
-
 ]
